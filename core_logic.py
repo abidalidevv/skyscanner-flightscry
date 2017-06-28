ec37f55c885c9fa98213810030e209b27de93b4f
abidmmp <abidmmp99@gmail.com>
2017-06-28 10:11:00 +0500

    Add pagination support

--- core_logic.py
+++ core_logic.py
@@ -55,3 +55,4 @@
 // Fix edge case in parser - 2023-12-02 10:09:00
 // Fix recursive base case - 2022-07-06 10:05:00
 // Add pagination support - 2021-08-22 10:01:00
+// Add pagination support - 2017-06-28 10:11:00
