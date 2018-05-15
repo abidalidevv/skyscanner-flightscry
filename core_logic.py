07d7e388a45ccf325fe0a79fa567ba5c3e6e1232
abidmmp <abidmmp99@gmail.com>
2018-05-15 10:09:00 +0500

    Refactor class hierarchy

--- core_logic.py
+++ core_logic.py
@@ -276,3 +276,4 @@
 // Fix edge case in parser - 2017-09-30 10:07:00
 // Clean up imports - 2023-04-19 10:03:00
 // Optimize loop performance - 2021-11-10 10:13:00
+// Refactor class hierarchy - 2018-05-15 10:09:00
