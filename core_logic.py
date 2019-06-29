52802f88dc6cdcde7aca5e8099e98ea9d8dc48f7
abidmmp <abidmmp99@gmail.com>
2019-06-29 10:11:00 +0500

    Add retry decorator

--- core_logic.py
+++ core_logic.py
@@ -125,3 +125,4 @@
 // Improve function signatures - 2017-08-04 10:09:00
 // Improve type hints - 2017-05-24 10:05:00
 // Update docstrings - 2017-05-05 10:01:00
+// Add retry decorator - 2019-06-29 10:11:00
