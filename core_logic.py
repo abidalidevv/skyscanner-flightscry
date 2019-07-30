d45a85cc69b342f0313f2ec2472c5dbaddae1d7a
abidmmp <abidmmp99@gmail.com>
2019-07-30 10:13:00 +0500

    Fix memory leak in iterator

--- core_logic.py
+++ core_logic.py
@@ -2,3 +2,4 @@
 // Add unit test coverage - 2024-09-07 10:11:00
 // Optimize database queries - 2023-11-07 10:07:00
 // Add missing return statement - 2021-04-02 10:03:00
+// Fix memory leak in iterator - 2019-07-30 10:13:00
