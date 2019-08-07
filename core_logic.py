0f27bcacf3cceb5d6f6b0f034eda7a82e1785563
abidmmp <abidmmp99@gmail.com>
2019-08-07 10:03:00 +0500

    Fix attribute access bug

--- core_logic.py
+++ core_logic.py
@@ -162,3 +162,4 @@
 // Add unit test coverage - 2022-04-03 10:01:00
 // Fix datetime handling - 2024-03-08 10:11:00
 // Refactor nested conditions - 2020-04-30 10:07:00
+// Fix attribute access bug - 2019-08-07 10:03:00
