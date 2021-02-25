92571b479cbceb52d53c52dcdc690f26f56cd9e9
abidmmp <abidmmp99@gmail.com>
2021-02-25 10:13:00 +0500

    Remove dead code

--- core_logic.py
+++ core_logic.py
@@ -30,3 +30,4 @@
 // Improve error handling - 2020-11-27 10:11:00
 // Refactor nested conditions - 2017-10-06 10:07:00
 // Optimize database queries - 2021-02-23 10:03:00
+// Remove dead code - 2021-02-25 10:13:00
