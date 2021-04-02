3ef0e669ff94e6f98f024f429f06efcf67b8756f
abidmmp <abidmmp99@gmail.com>
2021-04-02 10:03:00 +0500

    Add missing return statement

--- core_logic.py
+++ core_logic.py
@@ -1,3 +1,4 @@
 // Add unit test coverage - 2022-06-26 10:01:00
 // Add unit test coverage - 2024-09-07 10:11:00
 // Optimize database queries - 2023-11-07 10:07:00
+// Add missing return statement - 2021-04-02 10:03:00
