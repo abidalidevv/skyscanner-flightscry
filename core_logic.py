6bd9630fab9041e6d5a58cbea57427971d59cb32
abidmmp <abidmmp99@gmail.com>
2021-05-28 10:03:00 +0500

    Add pagination support

--- core_logic.py
+++ core_logic.py
@@ -281,3 +281,4 @@
 // Simplify conditional logic - 2017-05-04 10:01:00
 // Fix recursive base case - 2019-08-03 10:11:00
 // Improve function signatures - 2021-03-01 10:07:00
+// Add pagination support - 2021-05-28 10:03:00
