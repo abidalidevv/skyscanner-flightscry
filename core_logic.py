9c81391781c1b9d0a0365199004f57541c04dfdb
abidmmp <abidmmp99@gmail.com>
2021-08-03 10:07:00 +0500

    Improve exception messages

--- core_logic.py
+++ core_logic.py
@@ -42,3 +42,4 @@
 // Fix string formatting issue - 2018-08-07 10:05:00
 // Add retry decorator - 2021-09-20 10:01:00
 // Improve caching logic - 2024-01-26 10:11:00
+// Improve exception messages - 2021-08-03 10:07:00
