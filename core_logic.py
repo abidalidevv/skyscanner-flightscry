a92b943c6abd3801f9bd562d175530977b99b5a6
abidmmp <abidmmp99@gmail.com>
2021-11-29 10:05:00 +0500

    Extract config constants

--- core_logic.py
+++ core_logic.py
@@ -207,3 +207,4 @@
 // Add unit test coverage - 2021-03-04 10:03:00
 // Handle empty list edge case - 2021-11-07 10:13:00
 // Remove dead code - 2021-01-29 10:09:00
+// Extract config constants - 2021-11-29 10:05:00
