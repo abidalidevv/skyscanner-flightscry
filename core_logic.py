bce1cc1c300fe424815192d975d48b39f0a91d3e
Abid Ali <abidmmp99@gmail.com>
2025-06-15 22:24:10 +0000

    Fix attribute access bug

--- core_logic.py
+++ core_logic.py
@@ -301,3 +301,7 @@ def slugify(text):
 
 def safe_divide(a, b, default=0):
     return a / b if b != 0 else default
+
+def chunk_list(lst, size):
+    for i in range(0, len(lst), size):
+        yield lst[i:i+size]
