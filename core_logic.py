d5168feac977b1fa8bd0819fbcf528f1575eae66
Abid Ali <abidmmp99@gmail.com>
2025-11-10 13:04:21 +0000

    Improve function signatures

--- core_logic.py
+++ core_logic.py
@@ -326,3 +326,6 @@ def flatten(nested):
         if isinstance(item, list): result.extend(flatten(item))
         else: result.append(item)
     return result
+
+def format_date(dt):
+    return dt.strftime('%Y-%m-%d')
